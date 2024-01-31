dc6d90502ea16c482c31f032864ac4e5ebc98821
Theoabw <theo@wilenius.eu>
2024-01-31 22:38:13 +0200

    Changed homepage paragraph

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -15,12 +15,18 @@ function Homepage() {
                 </div>
                 <h1>Welcome to My CV Website</h1>
                 <div className="paragraph-container">
-                <p>
-                    Welcome to the digital hub of Theo Wilenius, an enthusiastic Computer Engineering student at Åbo
-                    Akademi. Here you'll find a blend of academic achievement and hands-on tech projects, with a special
-                    focus on web development and IT. Take a journey through my portfolio, experience, and the AI
-                    innovations that inspire me daily.
-                </p>
+                    <p>
+                        Hello! I'm Theo Wilenius. It's great to have you on my website. I'm currently studying computer engineering at Åbo Akademi University, diving into the fascinating world of programming, data analytics, and machine learning.
+                    </p>
+                    <p>
+                    Part of my journey involves working with our student association's website, where I contribute to maintaining and improving it using Django. It's an interesting experience that allows me to apply what I learn in a practical setting.
+                    </p>
+                    <p>
+                    Outside of university, I'm just enjoying life in Turku, Finland, participating in student activities, or just taking in the day.
+                    </p>
+                    <p>
+                    On this site, you'll find a bit about my projects and skills. If you're interested in tech, want to discuss a project, or just feel like saying hi, feel free to get in touch. Thanks for stopping by!
+                    </p>
             </div>
             </div>
         <UnderConstruction />
